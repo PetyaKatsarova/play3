134890128496eb83205185092e53ac8564655ba7
Katsarova <petya.katsarova@cgi.com>
2023-10-27 09:07:58 +0200

    register, confirm email from a link in email and login are complete

--- src/main/java/com/example/play3/controller/LoginController.java
+++ src/main/java/com/example/play3/controller/LoginController.java
@@ -1,12 +1,47 @@
 package com.example.play3.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.play3.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+//    @CrossOrigin(origins = "*")
+//    @GetMapping("/login")
+//    public String loginPage() {
+//        return "redirect:/loginPage.html";
+//    }
+
     @CrossOrigin(origins = "*")
-    @GetMapping("/login")
-    public String loginPage() {
-        return "redirect:/loginPage.html";
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> loginUser(@RequestBody Map<String, String> loginDetails) {
+        String username = loginDetails.get("username");
+        String password = loginDetails.get("password");
+
+        Map<String, Object> response = new HashMap<>();
+        try {
+            boolean isAuthenticated = loginService.authenticateUser(username, password);
+            if (isAuthenticated) {
+                response.put("success", true);
+                return ResponseEntity.ok(response);
+            } else {
+                response.put("success", false);
+                response.put("message", "Incorrect username or password");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+            }
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 }
